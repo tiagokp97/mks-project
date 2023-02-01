bcf406dcb6fc7a87f33bac11d71c5924b126747e
Tiago Pesch <tiago.pesch@gmail.com>
2023-02-01 13:11:07 -0300

    styles: Global styles

---
+++ src/styles/global.tsx
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+}
+
+:root {
+    --primary-blue: #0F52BA;
+    --primary-background: #E5E5E5;
+    --primary-white: #FFFFFF;
+    --primary-grey:  #2C2C2C;
+    --secondary-grey: #373737;
+    --border-grey: #BFBFBF;
+    --primary-footer: #EEEEEE;
+}
+
+body {
+    background: var(--primary-background);
+    color: black;
+
+
+}
+
+
+body, input, button, p, h1 {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 0.8rem;
+}
+
+button {
+    cursor: pointer;
+}
+
+a {
+    text-decoration: none;
+}
+
+`;
