50d0cbaffc83b47ee7eb9ca9f4c0bdb04e70babf
Tiago Pesch <tiago.pesch@gmail.com>
2023-02-01 12:55:09 -0300

    build: dependencies & project start.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Header from "./components/header"
+import GlobalStyles from './styles/global'
+import ProductsList from "./components/productList.tsx"
+import Footer from "./components/footer"
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function App() {
+
+  return (
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
