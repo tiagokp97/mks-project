d109290c1fe0828a4506bf310d51bf071a7b198b
Tiago Pesch <tiago.pesch@gmail.com>
2023-02-01 13:11:54 -0300

    feature: component footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,7 @@
+import { StyledFooter } from "./styled"
+
+export default function Footer() {
+    return (
+        <StyledFooter>MKS Sistemas © Todos os direitos reservados</StyledFooter>
+    )
+}
---
+++ src/components/footer/styled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const StyledFooter = styled.footer`
+  width: 100%;
+  background-color: var(--primary-footer);
+  color: black;
+  text-align: center;
+`
